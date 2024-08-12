b80f0378a65637b8f48784b03a66216143a2a71a
Your Name <210237@utxicotepec.edu.com>
2024-08-12 04:51:15 -0600

    Cambio responsivo en Graficos Direccion General

--- src/components/DireccionGeneral/GraficosDireccionG.vue
+++ src/components/DireccionGeneral/GraficosDireccionG.vue
@@ -1,14 +1,16 @@
 <template>
-    <div>
+    <div class="p-4">
       <!-- Encabezado con estilos de Tailwind CSS -->
-      <h1 class="text-2xl font-bold text-center text-gray-800 my-3">
+      <h1 class="text-2xl font-bold text-center text-gray-800 mb-6">
         Estadística de Dirección General
       </h1>
   
       <!-- Contenedor para el contenido con espacio a la izquierda en pantallas grandes -->
-      <div class="p-1 sm:ml-64">
-        <!-- Vista del enrutador para componentes de página -->
-        <router-view></router-view>
+      <div class="mb-6">
+        <div class="p-2 sm:p-4">
+          <!-- Vista del enrutador para componentes de página -->
+          <router-view></router-view>
+        </div>
       </div>
   
       <!-- Componente de gráfico -->
