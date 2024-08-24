4a01a1f84d904329994a968363f6ca5cbac99054
Romualdo <210714@utxicotepec.edu.mx>
2024-08-24 16:39:28 -0600

    Se importaron componentes necesarios para el uso de las graficas de pediatria

--- src/main.js
+++ src/main.js
@@ -3,9 +3,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+//Pediatria
+import Grafica1 from './components/Pediatria/Grafica1.vue'
+import Grafica2 from './components/Pediatria/Grafica2.vue'
+
 
 const app = createApp(App)
 
 app.use(router)
+
+// Pediatria
+app.component('Grafica1', Grafica1);
+app.component('Grafica2', Grafica2);
+
 
 app.mount('#app')
